c18e290954573abdd9d7f8a388870d84cabd8a6a
genNighty <mateusz.scecewicz@gmail.com>
2014-10-26 13:00:42 +0100

    Update MakeMagic.cs

--- MakeMagic.cs
+++ MakeMagic.cs
@@ -11,7 +11,6 @@ namespace BFS
     {
         string nazwa;
         string line;
-        string start;
         public int skierowany;
         public int size;
         string wp;
@@ -20,14 +19,24 @@ namespace BFS
 
         public void mayMagicBegin()
         {
-            Console.WriteLine("Podaj ścieżkę pliku");
+            Console.WriteLine("Podaj ścieżkę bezwzględną pliku");
 
             nazwa = Console.ReadLine();
             Console.WriteLine();
             StreamReader x = new StreamReader(nazwa, Encoding.UTF8);
-            start = x.ReadLine();
+            string start = x.ReadLine();
+            Node startowy = new Node(start);
+
             size = Int32.Parse(x.ReadLine());
             skierowany = Int32.Parse(x.ReadLine());
+
+            List<string>check = new List<string>();
+
+            Graph g = new Graph();
+            g.setRootNode(startowy);
+
+            Console.Write(size);
+            Console.ReadKey();
 
             line = x.ReadLine();
             while (line != null)
@@ -38,9 +47,29 @@ namespace BFS
                 line = line.Substring(line.IndexOf(" ") + 1);
                 w = line;
 
-                Console.WriteLine(wp + " " + wk + " " + w);
+                Node node = new Node(wp);
+                Node node2 = new Node(wk);
+            
+                if (!check.Contains(wp))
+                {
+                    check.Add(wp);
+                    g.addNode(node);
+                }
+                if (!check.Contains(wk))
+                {
+                    check.Add(wk);
+                    g.addNode(node2);
+                }
+
+                foreach (string s in check)
+                {
+                    Console.Write("LISTA NODOW: " + s + "\n");
+                }
+
+                g.connectNode(node, node2);
                 
                 line = x.ReadLine();
+                Console.ReadKey();
             }
 
             
